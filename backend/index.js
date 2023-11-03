2442a55892a88be7a50dd7201b522240b0945d7f
sanya chawla <chawlasanya327@gmail.com>
2023-11-03 20:31:52 +0530

    cors resolution

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin:"https://scriptologic.vercel.app/"
+  }
+));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
